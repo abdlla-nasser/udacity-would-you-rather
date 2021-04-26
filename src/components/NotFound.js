50fd7efcc6022bcc2bc08613ffac97402d6300af
abdlla-nasser <bedo2boy@gmail.com>
2021-04-26 03:48:46 +0200

    removed errors from not found

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,10 +1,7 @@
-import { useDispatch } from "react-redux";
 import { useLocation } from "react-router";
 import { CenterContainer } from "./CenterContainer";
 
 export function NotFound() {
-  const dispatch = useDispatch();
-  // dispatch({ type: "LOGOUT" });
   const location = useLocation();
   return location.pathname.includes("/question") ? (
     <CenterContainer>question not found</CenterContainer>
